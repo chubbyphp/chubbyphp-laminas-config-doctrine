6e412bc9a2666a98316ada93809ae3435749d854
Dominik Zogg <dominik.zogg@gmail.com>
2020-09-26 15:06:42 +0200

    add adder for orm configuration: namedQueries, namedNativeQueries, filters, all other add methods got also a setter

--- src/ServiceFactory/ORM/ConfigurationFactory.php
+++ src/ServiceFactory/ORM/ConfigurationFactory.php
@@ -12,14 +12,41 @@ final class ConfigurationFactory extends AbstractFactory
 {
     public function __invoke(ContainerInterface $container): Configuration
     {
+        $config = $this->resolveConfig($container->get('config')['doctrine']['orm']['configuration'] ?? []);
+
         $configuration = new Configuration();
 
-        $this->callSetters(
-            $container,
-            $configuration,
-            $this->resolveConfig($container->get('config')['doctrine']['orm']['configuration'] ?? [])
-        );
+        $this->callAdders($container, $configuration, $config);
+        $this->callSetters($container, $configuration, $config);
 
         return $configuration;
+    }
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    private function callAdders(ContainerInterface $container, Configuration $configuration, array &$config): void
+    {
+        $namedQueries = $this->resolveValue($container, $config['namedQueries'] ?? []);
+        $namedNativeQueries = $this->resolveValue($container, $config['namedNativeQueries'] ?? []);
+        $filters = $this->resolveValue($container, $config['filters'] ?? []);
+
+        unset($config['namedQueries'], $config['namedNativeQueries'], $config['filters']);
+
+        foreach ($namedQueries as $namedQuery) {
+            $configuration->addNamedQuery($namedQuery['name'], $namedQuery['dql']);
+        }
+
+        foreach ($namedNativeQueries as $namedNativeQuery) {
+            $configuration->addNamedNativeQuery(
+                $namedNativeQuery['name'],
+                $namedNativeQuery['sql'],
+                $namedNativeQuery['rsm']
+            );
+        }
+
+        foreach ($filters as $filter) {
+            $configuration->addFilter($filter['name'], $filter['className']);
+        }
     }
 }
--- tests/Unit/ServiceFactory/ORM/ConfigurationFactoryTest.php
+++ tests/Unit/ServiceFactory/ORM/ConfigurationFactoryTest.php
@@ -8,6 +8,7 @@ use Chubbyphp\Laminas\Config\Doctrine\ServiceFactory\ORM\ConfigurationFactory;
 use Chubbyphp\Mock\Call;
 use Chubbyphp\Mock\MockByCallsTrait;
 use Doctrine\ORM\Configuration;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\Persistence\Mapping\Driver\MappingDriver;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
@@ -26,6 +27,9 @@ final class ConfigurationFactoryTest extends TestCase
         /** @var MappingDriver $mappingDriver */
         $mappingDriver = $this->getMockByCalls(MappingDriver::class);
 
+        /** @var ResultSetMapping $resultSetMapping */
+        $resultSetMapping = $this->getMockByCalls(ResultSetMapping::class);
+
         /** @var ContainerInterface $container */
         $container = $this->getMockByCalls(ContainerInterface::class, [
             Call::create('get')->with('config')->willReturn([
@@ -35,10 +39,27 @@ final class ConfigurationFactoryTest extends TestCase
                             'metadataDriverImpl' => MappingDriver::class,
                             'proxyDir' => '/tmp/doctrine/orm/proxies',
                             'proxyNamespace' => 'DoctrineORMProxy',
+                            'namedQueries' => [
+                                ['name' => 'namedQuery', 'dql' => 'dql'],
+                            ],
+                            'namedNativeQueries' => [
+                                ['name' => 'namedNativeQuery', 'sql' => 'sql', 'rsm' => 'rsm'],
+                            ],
+                            'filters' => [
+                                ['name' => 'filter', 'className' => 'className'],
+                            ],
                         ],
                     ],
                 ],
             ]),
+            Call::create('has')->with('namedQuery')->willReturn(false),
+            Call::create('has')->with('dql')->willReturn(false),
+            Call::create('has')->with('namedNativeQuery')->willReturn(false),
+            Call::create('has')->with('sql')->willReturn(false),
+            Call::create('has')->with('rsm')->willReturn(true),
+            Call::create('get')->with('rsm')->willReturn($resultSetMapping),
+            Call::create('has')->with('filter')->willReturn(false),
+            Call::create('has')->with('className')->willReturn(false),
             Call::create('has')->with(MappingDriver::class)->willReturn(true),
             Call::create('get')->with(MappingDriver::class)->willReturn($mappingDriver),
             Call::create('has')->with('/tmp/doctrine/orm/proxies')->willReturn(false),
@@ -47,15 +68,23 @@ final class ConfigurationFactoryTest extends TestCase
 
         $factory = new ConfigurationFactory();
 
+        /** @var Configuration $service */
         $service = $factory($container);
 
         self::assertInstanceOf(Configuration::class, $service);
+
+        self::assertSame('dql', $service->getNamedQuery('namedQuery'));
+        self::assertSame(['sql', $resultSetMapping], $service->getNamedNativeQuery('namedNativeQuery'));
+        self::assertSame('className', $service->getFilterClassName('filter'));
     }
 
     public function testCallStatic(): void
     {
         /** @var MappingDriver $mappingDriver */
         $mappingDriver = $this->getMockByCalls(MappingDriver::class);
+
+        /** @var ResultSetMapping $resultSetMapping */
+        $resultSetMapping = $this->getMockByCalls(ResultSetMapping::class);
 
         /** @var ContainerInterface $container */
         $container = $this->getMockByCalls(ContainerInterface::class, [
@@ -67,11 +96,28 @@ final class ConfigurationFactoryTest extends TestCase
                                 'metadataDriverImpl' => MappingDriver::class,
                                 'proxyDir' => '/tmp/doctrine/orm/proxies',
                                 'proxyNamespace' => 'DoctrineORMProxy',
+                                'namedQueries' => [
+                                    ['name' => 'namedQuery', 'dql' => 'dql'],
+                                ],
+                                'namedNativeQueries' => [
+                                    ['name' => 'namedNativeQuery', 'sql' => 'sql', 'rsm' => 'rsm'],
+                                ],
+                                'filters' => [
+                                    ['name' => 'filter', 'className' => 'className'],
+                                ],
                             ],
                         ],
                     ],
                 ],
             ]),
+            Call::create('has')->with('namedQuery')->willReturn(false),
+            Call::create('has')->with('dql')->willReturn(false),
+            Call::create('has')->with('namedNativeQuery')->willReturn(false),
+            Call::create('has')->with('sql')->willReturn(false),
+            Call::create('has')->with('rsm')->willReturn(true),
+            Call::create('get')->with('rsm')->willReturn($resultSetMapping),
+            Call::create('has')->with('filter')->willReturn(false),
+            Call::create('has')->with('className')->willReturn(false),
             Call::create('has')->with(MappingDriver::class)->willReturn(true),
             Call::create('get')->with(MappingDriver::class)->willReturn($mappingDriver),
             Call::create('has')->with('/tmp/doctrine/orm/proxies')->willReturn(false),
@@ -80,8 +126,13 @@ final class ConfigurationFactoryTest extends TestCase
 
         $factory = [ConfigurationFactory::class, 'default'];
 
+        /** @var Configuration $service */
         $service = $factory($container);
 
         self::assertInstanceOf(Configuration::class, $service);
+
+        self::assertSame('dql', $service->getNamedQuery('namedQuery'));
+        self::assertSame(['sql', $resultSetMapping], $service->getNamedNativeQuery('namedNativeQuery'));
+        self::assertSame('className', $service->getFilterClassName('filter'));
     }
 }
